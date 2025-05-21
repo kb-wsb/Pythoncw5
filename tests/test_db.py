b270257e1ce0ff5d1cca362e4323b975a640a9fc
wrx91023 <wrx91023@student.wroclaw.merito.pl>
2025-05-21 19:08:32 +0200

    123

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,4 +1,9 @@
-import os
+import os, sys
+
+root = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
+if root not in sys.path:
+    sys.path.insert(0, root)
+
 import pytest
 from app import hello, add, subtract, multiply, divide
 from pymongo import MongoClient
